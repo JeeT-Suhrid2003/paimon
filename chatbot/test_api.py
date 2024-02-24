355013602eed6b080f8b5ea4fcaa4627f1dca437
JeeT-Suhrid2003 <suhridghosh2003@gmail.com>
2024-02-24 22:55:13 +0530

    adding the actual chatbot-ai

---
+++ chatbot/test_api.py
@@ -0,0 +1,25 @@
+from characterai import PyCAI
+
+client = PyCAI("d1a6d466ba7c0e41920d14e5cb339df67b318c5d")
+char = input('Enter CHAR: ')
+
+chat = client.chat.get_chat(char)
+
+participants = chat['participants']
+
+if not participants[0]['is_human']:
+    tgt = participants[0]['user']['username']
+else:
+    tgt = participants[1]['user']['username']
+
+while True:
+    message = input('You: ')
+
+    data = client.chat.send_message(
+        chat['external_id'], tgt, message
+    )
+
+    name = data['src_char']['participant']['name']
+    text = data['replies'][0]['text']
+
+    print(f"{name}: {text}")
